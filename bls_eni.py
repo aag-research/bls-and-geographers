4134d083a92dc6ae6cdd1ccf18c1477d1c739933
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-06-27 14:58:37 -0400

    Cleaned up bls_eni
    Code is clearer/easeir to read now. Changed a few variable names to names that are more relevant. This commit also includes tiger line shapefile

--- bls_eni.py
+++ bls_eni.py
@@ -18,8 +18,8 @@ import requests
 import json
 
 # Coline's and Eni's Directories
-folder = r'C:\Users\cdony\Google Drive\GitHub\bls-and-geographers'
-#folder = r'C:\Users\oawowale\Documents\GitHub\bls-and-geographers'
+#folder = r'C:\Users\cdony\Google Drive\GitHub\bls-and-geographers'
+folder = r'C:\Users\oawowale\Documents\GitHub\bls-and-geographers'
 os.chdir(folder)
 
 """
@@ -55,25 +55,31 @@ data_type = '01'            # Employment
 # The BLS State codes represent each state and its name as used in their databases 
 # see https://download.bls.gov/pub/time.series/sa/sa.state
 
+#Url request so we can get the states and state codes
 BLS_dictionary_url = r'https://download.bls.gov/pub/time.series/sa/sa.state'
 BLS_dictionary_textfile = requests.get(BLS_dictionary_url).text
 bls_states_db = {}
 
 for line in BLS_dictionary_textfile.split('\n')[1:]:
+  #Skip the first line loop and split to list by  each next line
   try: state_code, state_name = line.strip().split('\t')
+  #initializing variables state code and state name
   except: continue
   bls_states_db[state_code] = state_name
+  #adding items to bls_states_db: keys are state codes, values are state names
 
 # A2. Occupation codes: read occupation codes that relate to Geography occupation
-# The AAG collects salaraies from the BLS website for geography-related occupations
-# We use the AAG list of occupaiton
+# The AAG collects salaries from the BLS website for geography-related occupations
+# We use the AAG list of occupation
 
+#Same thing as url request above but instead it is for occupations
 BLS_dictionary_url = r'https://download.bls.gov/pub/time.series/oe/oe.occupation'
 BLS_dictionary_textfile = requests.get(BLS_dictionary_url).text
 bls_occupations_db = {}
 
 for line in BLS_dictionary_textfile.split('\n')[1:]:
   try: occupation_code_6digit, occupation_name = line.strip().split('\t')[:2]
+  #splits and strips at tab, saves both variables
   except: continue
   bls_occupations_db[occupation_code_6digit] = occupation_name
 
@@ -82,9 +88,12 @@ AAG_salary_data_textfile = open(AAG_salary_data_textfilename).readlines()
 headers = AAG_salary_data_textfile[0].split('\t')
 aag_occupations_db = {}
 
+
 for line in AAG_salary_data_textfile[1:]:
+  #Skips headers on textfile
   # The occupations in the salary data file are based on the BLS 8-digit codes
   occ_name_8digit, occ_code_8digit = line.split('\t')[:2]
+  #splits by tab
   if occ_code_8digit != '':
     # To make queries to the BLS API, we need the 6-digit code instead
     occ_code_6digit = occ_code_8digit.replace('-', '').replace('.', '')[:6]
@@ -93,20 +102,20 @@ for line in AAG_salary_data_textfile[1:]:
       occ_code_6digit = occ_code_6digit[:5] + '0'
       occ_name_6digit = bls_occupations_db[occ_code_6digit]
 
+    #adding the occupational codes the agg_occupations_db
     if occ_code_6digit not in aag_occupations_db:
       aag_occupations_db[occ_code_6digit] = { 'Main occupation name': occ_name_6digit,
                                               'Geography occupations': {occ_code_8digit : occ_name_8digit}}
     else: aag_occupations_db[occ_code_6digit]['Geography occupations'][occ_code_8digit] = occ_name_8digit
 
-#print (aag_occupations_db[occ_code_6digit])
 
-
+#Creating a list and text file of multiple series_ids
 series_id_list = []
-# A3. Create list of multiple series_ids
 series_ids_file = open('list_series_id.txt', 'w')
-
+#Initializing list AGG of occupations
 aag_occupations = list(aag_occupations_db.keys())
-#print(len(aag_occupations))
+
+#Initializing variable series_id that follows the BLS series_id format
 for state_code in bls_states_db:
   for occupation_code in aag_occupations:
     series_id = prefix + seasonal_code + area_type_code + state_code + area_code + industry_code + occupation_code + data_type
@@ -114,36 +123,25 @@ for state_code in bls_states_db:
     series_ids_file.write(series_id + '\n')
 series_ids_file.close()
 
-# for series in series_id_list:
-#   if series[17:23] > series[:-1]:
-#     series_id_list.(series, 0)
-# print(series_id_list)
-
-#print(series_id[17:23])
 # A4. Split the list into chucks of 50 series ids
 series_ids = [series_id.strip() for series_id in open('list_series_id.txt').readlines()]
 series_ids_chunks = [series_ids[i:i+50] for i in range(0, len(series_ids), 50)]
 
-# B. (Years) What years do we want to extract data from?
+# B. The years we want to extract from
 startyear = 2018
 endyear = 2018
 
 # C. Make Requests to the BLS API, using multiple series ids
 
-# BLS API keys
+# BLS API keys:
 bls_api_key_Eni = 'de6366639eb64fa79045c9071a080dd5'
 #bls_api_key_Coline = '41d57752042240da84a71fd2ba7c748d'
 
 # BLS API query
-
-#I wanted to make sure my edits were working so I changed the chunk to 8
-#the out put of text file shows two different states and how the state name changes depending
-#depending on the state_code within the series_id
-data_query = json.dumps({"seriesid": series_ids_chunks[1],
+data_query = json.dumps({"seriesid": series_ids_chunks[0],
                          "startyear":startyear,
                          "endyear":endyear,
                          "registrationkey": bls_api_key_Eni})
-#print(data_query)
 
 # BLS API location
 bls_api_location = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
@@ -157,9 +155,11 @@ post_request = requests.post(bls_api_location, data=data_query, headers=headers)
 # Get the API response as text and convert it to a JSON dictionary
 get_response = json.loads(post_request.text)
 
-series_ids_value_textfile = open('series_id_value.txt', 'w')
-series_ids_value_textfile.write('State\t' + '\t'.join(aag_occupations))
+#Creating new text file in a suitable format for joining attribute tables TIGER Line Shapefiles
+state_occupational_employment_textfile = open('state_occupational_employment.txt', 'w')
+state_occupational_employment_textfile.write('State\t' + '\t'.join(aag_occupations))
 
+#Starting Request
 state_values = []
 for series in get_response['Results']['series']:
   series_id = series['seriesID']
@@ -167,18 +167,14 @@ for series in get_response['Results']['series']:
   state_name = bls_states_db[state_code]
   occupation_code_6digit = series_id[17:23]
   occupation_index = aag_occupations.index(occupation_code_6digit)
-  #occupation_name_6digit = aag_occupations_db[occupation_code_6digit]['Main occupation name']
-  #aag_occupations = aag_occupations_db[occupation_code_6digit]['Geography occupations']
   try: employment = series['data'][0]['value'].replace('-', 'no est.')
   except: employment = 'none'
-  #occupation_name = occupation_name_6digit + ' (includes:' + ','.join(list(aag_occupations.values())) + ')'
   if state_name not in state_values:
-    series_ids_value_textfile.write('\t'.join(state_values) + '\n')
+    state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
     state_values = [state_name] + ['*']*len(aag_occupations)
   state_values[occupation_index + 1] = employment
-series_ids_value_textfile.write('\t'.join(state_values) + '\n')
+state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
 
-#print(aag_occupations_db)
 del requests
 del json
-series_ids_value_textfile.close()
+state_occupational_employment_textfile.close()
