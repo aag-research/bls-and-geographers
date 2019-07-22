b8ed01eb3546616e3bfc32659f6e1fefa1206a85
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-07-22 12:14:26 -0400

    Reading from occupations textfile

--- bls_eni.py
+++ bls_eni.py
@@ -64,13 +64,9 @@ bls_states_values_db = {}
 
 #Creating state dictionary
 for line in BLS_dictionary_textfile.split('\n')[1:]:
-  #Skip the first line loop and split to list by  each next line
   try: state_code, state_name = line.strip().split('\t')
-  #initializing variables state code and state name
   except: continue
   bls_states_db[state_code] = state_name
-
-  #adding items to bls_states_db: keys are state codes, values are state names
 
 
 #Same thing as url request above but instead it is for occupations
@@ -80,7 +76,6 @@ bls_occupations_db = {}
 
 for line in BLS_dictionary_textfile.split('\n')[1:]:
   try: occupation_code_6digit, occupation_name = line.strip().split('\t')[:2]
-  #splits and strips at tab, saves both variables
   except: continue
   bls_occupations_db[occupation_code_6digit] = occupation_name
 
@@ -91,7 +86,6 @@ aag_occupations_db = {}
 
 
 for line in AAG_salary_data_textfile[1:]:
-  #Skips headers on textfile
   # The occupations in the salary data file are based on the BLS 8-digit codes
   occ_name_8digit, occ_code_8digit = line.split('\t')[:2]
   #splits by tab
@@ -99,6 +93,7 @@ for line in AAG_salary_data_textfile[1:]:
     # To make queries to the BLS API, we need the 6-digit code instead
     occ_code_6digit = occ_code_8digit.replace('-', '').replace('.', '')[:6]
     try: occ_name_6digit = bls_occupations_db[occ_code_6digit]
+    #adding a zero to the end of the 6 digit codes so it is the correct length
     except:
       occ_code_6digit = occ_code_6digit[:5] + '0'
       occ_name_6digit = bls_occupations_db[occ_code_6digit]
@@ -109,16 +104,11 @@ for line in AAG_salary_data_textfile[1:]:
                                               'Geography occupations': {occ_code_8digit : occ_name_8digit}}
     else: aag_occupations_db[occ_code_6digit]['Geography occupations'][occ_code_8digit] = occ_name_8digit
 
-
 #Creating a list and text file of multiple series_ids
 series_id_list = []
 series_ids_file = open('list_series_id.txt', 'w')
 #Initializing list AGG of occupations
 aag_occupations = list(aag_occupations_db.keys())
-
-#Adding occupations to state database
-
-
 
 #Initializing variable series_id that follows the BLS series_id format
 for state_code in bls_states_db:
@@ -139,23 +129,27 @@ endyear = 2018
 # C. Make Requests to the BLS API, using multiple series ids
 
 # BLS API keys:
-#bls_api_key_Eni_3 = '8649eca48fd747478b7ba9a7095b2473'
-bls_api_key_Eni_2 = '9954b4145b514eae81c6fe9a93739299'
+bls_api_key_Eni_3 = '8649eca48fd747478b7ba9a7095b2473'
+#bls_api_key_Eni_2 = '9954b4145b514eae81c6fe9a93739299'
 #bls_api_key_Eni = 'de6366639eb64fa79045c9071a080dd5'
 #bls_api_key_Coline = '41d57752042240da84a71fd2ba7c748d'
 #bls_api_key = '41d57752042240da84a71fd2ba7c748d'
 
 
 #Creating new text file in a suitable format for joining attribute tables TIGER Line Shapefiles
+'''
 state_occupational_employment_textfile = open('state_occupational_employment.txt', 'w')
-#state_occupational_employment_textfile.write('State\t' + '\t'.join(aag_occupations))
+state_occupational_employment_textfile.write('State\t' + '\t'.join(aag_occupations))
 state_values = []
 state_values_appending = []
+'''
+#Part 1: Requesting Occupational Data
+'''
 for series_ids_chunk in series_ids_chunks:
   data_query = json.dumps({"seriesid": series_ids_chunk,
                          "startyear": startyear,
                          "endyear": endyear,
-                         "registrationkey": bls_api_key_Eni_2})
+                         "registrationkey": bls_api_key_Eni_3})
   # BLS API location
   bls_api_location = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
 
@@ -175,25 +169,73 @@ for series_ids_chunk in series_ids_chunks:
     state_name = bls_states_db[state_code]
     occupation_code_6digit = series_id[17:23]
     occupation_index = aag_occupations.index(occupation_code_6digit)
-    try: employment = series['data'][0]['value'].replace('-', 'no est.')
-    except: employment = 'none'
-    #adding state names to new dictionary with values
+    try:
+      employment_text = series['data'][0]['value'].replace('-', 'no est.')
+      try:
+        employment_int = int(employment_text)
+      except ValueError:
+        employment_int = 0
+    except:
+      employment_int = 0
+      employment_text = 'none'
     if state_name not in bls_states_values_db:
-      bls_states_values_db[state_name]={}
-    if employment and occupation_code_6digit not in bls_states_values_db:
-      bls_states_values_db[state_name][occupation_code_6digit]=employment
+      bls_states_values_db[state_name] = {}
+    if employment_text and occupation_code_6digit not in bls_states_values_db:
+      bls_states_values_db[state_name][occupation_code_6digit] = employment_int
+        #adding state names to new dictionary with values
+
 
     if state_name not in state_values:
-      #state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
+      state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
       state_values = [state_name] + ['*']*len(aag_occupations)
-    state_values[occupation_index + 1] = employment
-#state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
+    state_values[occupation_index + 1] = employment_text
+state_occupational_employment_textfile.write('\t'.join(state_values) + '\n')
 state_occupational_employment_textfile.close()
-state_occupational_values_list = [[] for occ_value in range(len(bls_states_values_db))]
-state_occupational_values_list2=[]
+'''
+#Part 2: Comparing Occupational Data
+state_employment_textfile_reading = open('occupational_employment_values_for_reading.txt').readlines()
+occupation_codes_text = state_employment_textfile_reading[0].strip('State\t')
+# for state in bls_states_db.values():
+#   bls_states_values_db[state]={}
+for state_data in state_employment_textfile_reading:
+  occupation_codes = occupation_codes_text.split('\t')
+  state_names = state_data.split('\t')[0]
+  if state_names != 'State':
+    clean_state_name = state_names
+    bls_states_values_db[clean_state_name]={}
+    for code in occupation_codes:
+      #bls_states_values_db[clean_state_name][code]={}
+      for employment_value in range(len(state_employment_textfile_reading)):
+        values = state_employment_textfile_reading[employment_value].split('\t')
+        bls_states_values_db[clean_state_name][code] = values[employment_value]
+        print(values[1])
+
+
+#bls_states_values_db[state_names][employment_values]={}
+# for employment_value in state_employment_textfile_reading[1:]:
+  #   state_values=employment_value.split('\t')
+  #   bls_states_values_db[state_values[0]][occupation_code] = {}
+
+# for state in bls_states_db.values():
+#   bls_states_values_db[state]={}
+#   for occupation_code in headers_employment_textfile[1:]:
+#     bls_states_values_db[state][occupation_code]='value'
+
+#state_occupational_values_list = [[] for occ_value in range(len(bls_states_values_db))]
 state_occupational_value={}
 #state_occupation_values = bls_states_values_db
 #Changing values to integer so they can be compared:
+# state_occupational_values_list=[]
+# for occupation, employment_data in bls_states_values_db.items():
+#     for occupation_code, occupation_value in employment_data.items():
+#         state_occupational_values_list.append((occupation_value, occupation_code))
+# top_5=sorted(state_occupational_values_list,reverse=True)[:5]
+
+
+
+
+
+'''
 for value in bls_states_values_db.values():
     for key, occupation_value in value.items():
         if occupation_value.isdigit():
@@ -202,13 +244,13 @@ for value in bls_states_values_db.values():
             value[key] = occupation_value
             state_occupational_values_list2.append(new_occupation_value)
             for list in range(0,95):
-              state_occupational_values_list[list].append(state_occupational_values_list2[0:-1:95])
+              state_occupational_values_list[0:].append(state_occupational_values_list2[0:-1:95])
         else:
           value[key]=0
           state_occupational_values_list2.append(0)
           for list in range(len(state_occupational_values_list)):
-            state_occupational_values_list[list].append(state_occupational_values_list2[list])
-        # state_occupational_values_list = [[] for occ_value in range(len(bls_states_values_db))]
+            state_occupational_values_list[0].append(state_occupational_values_list2[0:-1:95])
+        # state_occupational_values_list = [[] for occ_value in range(len(bls_states_values_db))]'''
         # state_occupational_values_list.append(new_occupation_value)
 
     #for num in range(0,95):
